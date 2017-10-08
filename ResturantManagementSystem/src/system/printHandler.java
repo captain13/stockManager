4094488542b852561f19a701f0a851ed02fc2b9f
AndrewSchwabe <swabe@live.co.za>
2017-10-08 22:37:57 +0200

    Update
    -Code cleanup

--- ResturantManagementSystem/src/system/printHandler.java
+++ ResturantManagementSystem/src/system/printHandler.java
@@ -15,14 +15,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Arrays;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 /**
@@ -35,35 +27,17 @@ public class printHandler {
     String Heading;
     String name;
     dbManager db = new dbManager();
+    String currentUsersHomeDir = System.getProperty("user.home");
+    String file_name = currentUsersHomeDir + File.separator + "Documents\\NetBeansProjects\\stockManager\\ResturantManagementSystem\\src\\docs\\Print.pdf";
 
-    public void printTextToPDF() throws ClassNotFoundException, InstantiationException, SQLException {
-        name = "Inventory Table " + "\n" + "\n";
-        inventory = Arrays.deepToString(db.getInventoryData()).replaceAll("[^\\p{javaSpaceChar}{2.a-zA-Z0-9&,}]", "\n") + "";
-        Heading = "Inventory ID, Item Name, Category, Quantity(g), Item Threshold, Item Limit \n ";
-        String currentUsersHomeDir = System.getProperty("user.home");
-        String file_name = currentUsersHomeDir + File.separator + "Documents\\NetBeansProjects\\stockManager\\ResturantManagementSystem\\src\\docs\\Print.pdf";
-        String url = "jdbc:mysql://localhost:3306/resturantdb";
-        String username = "root";
-        String password = "root";
-        String driver = "com.mysql.jdbc.Driver";
-
+    public void printTextToPDF() {
         Document document = new Document();
         try {
             PdfWriter.getInstance(document, new FileOutputStream(new File(file_name)));
             //open
             document.open();
-            Paragraph p = new Paragraph();
-            p.add(name);
-            p.add(Heading);
-            p.add(inventory);
             PdfPTable table = new PdfPTable(7);
             PdfPCell table_cell;
-
-            Class.forName(driver).newInstance();
-            Connection conn = DriverManager.getConnection(url, username, password);
-            Statement s = conn.createStatement();
-            ResultSet query = s.executeQuery("SELECT inventoryID, item, category, qty, itemThreshold, itemLimit, itemCost FROM inventory");
-
             table.addCell((new Paragraph("Inventory ID")));
             table.addCell((new Paragraph("Item")));
             table.addCell((new Paragraph("Category")));
@@ -71,43 +45,35 @@ public class printHandler {
             table.addCell((new Paragraph("Item Threshold")));
             table.addCell((new Paragraph("Item Limit")));
             table.addCell((new Paragraph("Item Cost")));
-
-            while (query.next()) {
-
-                String InventoryID = query.getString("inventoryID");
+            for (int i = 0; i < db.getInventoryData().length; i++) {
+                String InventoryID = db.getInventoryData()[i][0].toString();
                 table_cell = new PdfPCell(new Phrase(InventoryID));
                 table.addCell(table_cell);
-                String item = query.getString("item");
+                String item = db.getInventoryData()[i][1].toString();
                 table_cell = new PdfPCell(new Phrase(item));
                 table.addCell(table_cell);
-                String category = query.getString("category");
+                String category = db.getInventoryData()[i][2].toString();
                 table_cell = new PdfPCell(new Phrase(category));
                 table.addCell(table_cell);
-                String qty = query.getString("qty");
+                String qty = db.getInventoryData()[i][3].toString();
                 table_cell = new PdfPCell(new Phrase(qty));
                 table.addCell(table_cell);
-                String itemThreshold = query.getString("itemThreshold");
+                String itemThreshold = db.getInventoryData()[i][4].toString();
                 table_cell = new PdfPCell(new Phrase(itemThreshold));
                 table.addCell(table_cell);
-                String itemLimit = query.getString("itemLimit");
+                String itemLimit = db.getInventoryData()[i][5].toString();
                 table_cell = new PdfPCell(new Phrase(itemLimit));
                 table.addCell(table_cell);
-                String itemCost = query.getString("itemCost");
+                String itemCost = db.getInventoryData()[i][6].toString();
                 table_cell = new PdfPCell(new Phrase(itemCost));
                 table.addCell(table_cell);
             }
-
             document.add(table);
             document.close();
-            query.close();
-            s.close();
-            conn.close();
-
             JOptionPane.showMessageDialog(null, "Your table has been saved to PDF");
         } catch (FileNotFoundException | DocumentException e) {
-        } catch (IllegalAccessException ex) {
-            Logger.getLogger(printHandler.class.getName()).log(Level.SEVERE, null, ex);
         }
+        
 
         //opens pdf
         if (Desktop.isDesktopSupported()) {
