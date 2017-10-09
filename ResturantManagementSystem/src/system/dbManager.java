12b3e7ab38a0d6c92eea71af7852fe14c0b5a13b
AndrewSchwabe <swabe@live.co.za>
2017-10-09 23:10:22 +0200

    Secondary Back-up restored

--- ResturantManagementSystem/src/system/dbManager.java
+++ ResturantManagementSystem/src/system/dbManager.java
@@ -566,6 +566,48 @@ public class dbManager {
         }
         return supplierName;
     }
+    
+    public Object[][] getReprintReceipt(String date, String time) { 
+        Object[][] rowData = null;
+        try {
+            Connection conn = DriverManager.getConnection(url, username, password);
+            Statement s = conn.createStatement();
+            String query = "SELECT * FROM receipt WHERE date = '" + date + "' AND time = '" + time + "'";
+            ResultSet rs = s.executeQuery(query);
+            ResultSetMetaData metaData = rs.getMetaData();
+            int columnCount = metaData.getColumnCount();
+            int rowCount = 0;
+            while (rs.next()) {
+                rowCount++;
+            }
+            rs = s.executeQuery(query);
+            rowData = new Object[rowCount][columnCount];
+            int i = 0;
+            while (rs.next()) {
+                rowData[i][0] = rs.getObject(1);
+                System.out.println(rowData[i][0]);
+                rowData[i][1] = rs.getObject(2);
+                rowData[i][2] = rs.getObject(3);
+                rowData[i][3] = rs.getObject(4);
+                rowData[i][4] = rs.getObject(5);
+                rowData[i][5] = rs.getObject(6);
+                rowData[i][6] = rs.getObject(7);
+                System.out.println(rowData[i][1]);
+                System.out.println(rowData[i][2]);
+                System.out.println(rowData[i][3]);
+                System.out.println(rowData[i][4]);
+                System.out.println(rowData[i][5]);
+                System.out.println(rowData[i][6]);
+                i++;
+            }
+            rs.close();
+            s.close();
+            conn.close();
+            } catch (SQLException exc) {
+                
+            }
+        return rowData;
+    }
 
     //delete later
     public void showActiveEmp() {
